8c8fdf9714f8105af24eb9930990e52b5f9f9abc
laolarou1 <782848448@qq.com>
2023-08-23 14:27:19 +0800

    修改cookie, 以获取全部影片而不是默认的仅有磁力连接的影片

--- Config/Config.py
+++ Config/Config.py
@@ -33,5 +33,6 @@ HEADERS = {
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'cookie': 'existmag=all'
 
 }
